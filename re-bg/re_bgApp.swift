e6d645f00763d5aa3e77f96c5f202b7ec610b8cc
largoudigital-dot <largou.digital@gmail.com>
2026-01-18 17:27:01 +0100

    Initial Commit

---
+++ re-bg/re_bgApp.swift
@@ -0,0 +1,17 @@
+//
+//  re_bgApp.swift
+//  re-bg
+//
+//  Created by Largou on 18.01.26.
+//
+
+import SwiftUI
+
+@main
+struct re_bgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ re-bgTests/re_bgTests.swift
@@ -0,0 +1,17 @@
+//
+//  re_bgTests.swift
+//  re-bgTests
+//
+//  Created by Largou on 18.01.26.
+//
+
+import Testing
+@testable import re_bg
+
+struct re_bgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
