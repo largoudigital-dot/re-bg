fc875b4ae669080bca106565bf3b55f060cdd68a
largoudigital-dot <largou.digital@gmail.com>
2026-01-25 13:47:02 +0100

    UI: Fehlende Foto-Standard-Seitenverhältnisse (3:2, 2:3, 3:4) hinzugefügt

--- re-bg/CanvasTabView.swift
+++ re-bg/CanvasTabView.swift
@@ -16,6 +16,9 @@ enum AspectRatio: String, CaseIterable, Identifiable {
     case apple55 = "5.5''"
     case apple58 = "5.8''"
     case fourThree = "4:3"
+    case threeFour = "3:4"
+    case threeTwo = "3:2"
+    case twoThree = "2:3"
     case original = "Original"
     case custom = "Eigene"
     
@@ -29,6 +32,8 @@ enum AspectRatio: String, CaseIterable, Identifiable {
         case .sixteenNine: return "rectangle.ratio.16.to.9"
         case .apple55, .apple58: return "apple-icon"
         case .fourThree: return "facebook"
+        case .threeFour: return "rectangle.portrait"
+        case .threeTwo, .twoThree: return "rectangle.ratio.3.to.2"
         case .original: return "aspectratio"
         case .custom: return "slider.horizontal.2.square"
         }
@@ -54,6 +59,9 @@ enum AspectRatio: String, CaseIterable, Identifiable {
         case .square: return 1.0
         case .fourFive: return 4.0 / 5.0
         case .fourThree: return 4.0 / 3.0
+        case .threeFour: return 3.0 / 4.0
+        case .threeTwo: return 3.0 / 2.0
+        case .twoThree: return 2.0 / 3.0
         case .sixteenNine: return 16.0 / 9.0
         case .nineSixteen: return 9.0 / 16.0
         case .apple55: return 9.0 / 16.0
